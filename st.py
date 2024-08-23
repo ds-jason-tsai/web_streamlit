0a22181638a078e9ee31cbd7da641f5bd6ce2bdb
Jason TSAI <jason.tsai.0604@gmail.com>
2024-08-23 16:48:41 +0800

    commit

--- st.py
+++ st.py
@@ -11,6 +11,9 @@ st.header('啟元食品行')
 # 製造間距
 st.write(f"")
 st.write(f"")
+
+st.image(https://lh5.googleusercontent.com/p/AF1QipNF0A-hAXYtaPUHDXzESGzBftZsF7spDhSKmLas=s774-k-no)
+
 st.write(f"")
 st.write(f"")
 
