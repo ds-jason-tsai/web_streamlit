c466953716ff03ed4509351f350574e1e3361fb7
Jason TSAI <jason.tsai.0604@gmail.com>
2024-08-23 16:35:42 +0800

    commit

--- st.py
+++ st.py
@@ -14,13 +14,13 @@ st.write(f"")
 st.write(f"")
 st.write(f"")
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
     st.image("https://lh5.googleusercontent.com/p/AF1QipNF0A-hAXYtaPUHDXzESGzBftZsF7spDhSKmLas=w203-h114-k-no")
-
 with col2:
     st.write("啓懋食品行為南北雜貨、烘焙食材，餐廳飯店食品材供應商我們重視每一位員工，除了有良好工作環境、也提供學習及成長的空間，歡迎優秀的朋友一起加入啓懋食品行的工作行列。")
-
+with col3:
+    st.write("")
 
 
 st.write(f"")
