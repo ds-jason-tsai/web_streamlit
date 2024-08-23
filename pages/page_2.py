663c3d1298825ea2cc7ede2c5f7a0bd7dceec477
Jason TSAI <jason.tsai.0604@gmail.com>
2024-08-23 09:25:34 +0800

    commit

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
+# 瀏覽器頁籤
+st.set_page_config(page_title='糖類產品')
+
 # Sidebar navigation
 st.sidebar.page_link('st.py', label='公司介紹')
 st.sidebar.page_link('pages/page_2.py', label='糖類')
-
-# 瀏覽器頁籤
-st.set_page_config(page_title='糖類產品')
 
 # 標題
 st.title('糖類')
