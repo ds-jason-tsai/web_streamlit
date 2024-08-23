26580ba6c2e6d03d7b21f700e3e3cf0132144222
Jason TSAI <jason.tsai.0604@gmail.com>
2024-08-23 10:05:38 +0800

    commit

--- pages/page_2.py
+++ pages/page_2.py
@@ -15,7 +15,9 @@ st.sidebar.page_link('pages/page_2.py', label='糖類')
 left_column, right_column = st.columns(2)
 
 with left_column:
+    st.header('圖片')
     st.write('左側')
 
 with right_column:
+    st.header('商品介紹')
     st.write('右側')
